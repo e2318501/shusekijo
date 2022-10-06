2a2eec16451c4f97d56e0c89918a9c6b9425624d
tsuoihito <74692297+tsuoihito@users.noreply.github.com>
2022-10-06 11:07:26 +0000

    refactor: remove diffuse code

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,17 @@
-import {
-    Channel,
-    Client,
-    Collection,
-    Intents,
-    Snowflake,
-    TextChannel,
-    Webhook,
-} from "discord.js";
-import log4js from "log4js";
+import { Client, Intents, TextChannel } from "discord.js";
 import * as dotenv from "dotenv";
-
-const logger = log4js.getLogger();
-logger.level = "info";
 
 dotenv.config();
 
-const collectChannelId: string | undefined = process.env.COLLECT_CHANNEL_ID;
-const botToken: string | undefined = process.env.BOT_TOKEN;
+const collectChannelId = process.env.COLLECT_CHANNEL_ID;
+const botToken = process.env.BOT_TOKEN;
 
 if (collectChannelId === undefined) {
-    logger.error("Channel ID is not specified.");
-    process.exit(1);
-}
-if (botToken === undefined) {
-    logger.error("Bot token is not specified.");
+    console.error("Channel ID is not specified.");
     process.exit(1);
 }
 
-const bot: Client = new Client({
+const bot = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
@@ -37,7 +21,7 @@ const bot: Client = new Client({
 });
 
 bot.on("ready", (bot) => {
-    logger.info(
+    console.info(
         `Logged in as ${bot.user.username} on ${bot.guilds.cache.size} servers.`
     );
     bot.user.setPresence({
@@ -55,36 +39,31 @@ bot.on("messageCreate", async (message) => {
         message.content === "" &&
         message.embeds.length === 0 &&
         message.attachments.size === 0
-    ) {
+    )
         return;
-    }
-    if (message.member === null) return;
 
-    const collectChannel: Channel | undefined =
-        message.guild?.channels.cache.get(collectChannelId);
+    const collectChannel = message.guild?.channels.cache.get(collectChannelId);
     if (collectChannel === undefined) return;
 
     if (!(collectChannel instanceof TextChannel)) return;
-    const webhooks: Collection<Snowflake, Webhook> =
-        await collectChannel.fetchWebhooks();
 
-    let webhook: Webhook | undefined = webhooks.find(
-        (wh) => wh.owner === bot.user
-    );
-    if (webhook === undefined) {
-        webhook = await collectChannel.createWebhook("chat-collector");
-    }
+    try {
+        const webhooks = await collectChannel.fetchWebhooks();
+        const webhook =
+            webhooks.find((wh) => wh.owner === bot.user) ||
+            (await collectChannel.createWebhook("chat-collector"));
 
-    await webhook
-        .send({
+        await webhook.send({
             content: message.content === "" ? undefined : message.content,
-            username: message.member.displayName,
-            avatarURL: message.member.displayAvatarURL(),
+            username: message.member?.displayName,
+            avatarURL: message.member?.displayAvatarURL(),
             embeds: message.embeds,
             files: Array.from(message.attachments.values()),
             allowedMentions: { repliedUser: false },
-        })
-        .catch((error) => logger.error(error));
+        });
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 bot.login(botToken);
