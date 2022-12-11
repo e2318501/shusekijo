c3b7f119a8c9a306b9dcbcaccd970618c74510b1
tsuoihito <tsuoihito@outlook.jp>
2022-12-11 13:47:30 +0900

    feat!: record message as Embed

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-import { Client, GatewayIntentBits, TextChannel } from "discord.js";
+import {
+    Client,
+    EmbedBuilder,
+    GatewayIntentBits,
+    TextChannel,
+} from "discord.js";
 import * as dotenv from "dotenv";
 
 dotenv.config();
@@ -11,7 +16,7 @@ if (collectChannelId === undefined) {
     process.exit(1);
 }
 
-const client = new Client({
+const client: Client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
@@ -30,24 +35,7 @@ client.on("ready", (bot) => {
 });
 
 client.on("messageCreate", async (message) => {
-    if (message.author === client.user) return;
-    if (message.channel.id === collectChannelId) return;
-
-    const isEmpty = () =>
-        message.content === "" &&
-        message.embeds.length === 0 &&
-        message.attachments.size === 0;
-
-    let timeout = false;
-    setTimeout(() => {
-        timeout = true;
-    }, 1000);
-
-    while (isEmpty() && !timeout) {
-        await new Promise((r) => setTimeout(r, 10));
-    }
-
-    if (isEmpty()) return;
+    if (message.author === client.user || message.member === null) return;
 
     const collectChannel = message.guild?.channels.cache.get(collectChannelId);
     if (collectChannel === undefined) return;
@@ -59,12 +47,39 @@ client.on("messageCreate", async (message) => {
         webhooks.find((wh) => wh.owner === client.user) ||
         (await collectChannel.createWebhook({ name: "chat-collector" }));
 
+    const description = message.content === "" ? null : message.content;
+    const imageUrl =
+        message.attachments.find(
+            (a) => a.contentType?.match(/image\/.*/) != null
+        )?.url || null;
+    if (description === null && imageUrl === null) return;
+
+    const embed = new EmbedBuilder()
+        .setAuthor({
+            name: message.member.displayName,
+            iconURL: message.member.displayAvatarURL() || undefined,
+        })
+        .setDescription(description)
+        .addFields([
+            {
+                name: "Channel",
+                value: `<#${message.channel.id}>`,
+                inline: true,
+            },
+            {
+                name: "Jump",
+                value: `[Click here](${message.url})`,
+                inline: true,
+            },
+        ])
+        .setImage(imageUrl)
+        .setTimestamp();
+
     await webhook.send({
-        content: message.content,
-        username: message.member?.displayName,
-        avatarURL: message.member?.displayAvatarURL(),
-        embeds: message.embeds,
-        files: Array.from(message.attachments.values()),
+        username: "Gyotaku",
+        avatarURL:
+            "https://1.bp.blogspot.com/-ztMJJI7R2-M/WQA9_fgWESI/AAAAAAABD4Q/x8kHmIQRAzgy24MTK0zNMYEUTyXN_vS1QCLcB/s400/fishing_gyotaku.png",
+        embeds: [embed],
         allowedMentions: { repliedUser: false },
     });
 });
